13a692088fc978815c026d2cb56a0e0e0c5469c7
rookiemo <frozenprince.k@gmail.com>
2017-05-06 02:34:01 +0700

    Make room navigation, room component and game controller

---
+++ Assets/Script/Exit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.SerializableAttribute]
+public class Exit{
+
+	public string keyString;
+	public string exitDescription;
+	public Room valueRoom;
+}
---
+++ Assets/Script/RoomNavigation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNavigation : MonoBehaviour {
+	public Room currentRoom;
+
+	GameController controller;
+
+	void Awake(){
+		controller = GetComponent<GameController>();
+	}
+
+	public void UnpackExitsInRoom(){
+		for(int i=0; i<currentRoom.exits.Length; i++){
+			controller.interationDescriptionsInRoom.Add(currentRoom.exits[i].exitDescription);
+		}
+	}
+}
